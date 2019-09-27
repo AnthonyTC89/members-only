aa24dceca0ace61c5c36cc5316250662f49bd3c4
AnthonyTC89 <ptonyptc@gmail.com>
2019-09-27 17:52:55 -0500

    destroy route fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,15 +30,17 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+    
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   private
 
     def remember
       remember_token  = User.new_token
       self.remember_digest = User.digest(remember_token)
     end
-    
-    def forget
-      update_attribute(:remember_digest, nil)
-    end
+
 
 end
