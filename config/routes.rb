ec4b8d141b63b8cbac16c2fe1d820db6f016e268
matheus-fls <matheus_fls@yahoo.com.br>
2019-10-01 16:46:11 -0300

    Update header, add signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
+  get '/signup', to: 'user#new'
+  post '/signup', to: 'user#create'
   root 'posts#index'
-  resources :users
+  resources :user
   resources :sessions, only: %i[new create destroy]
   resources :posts, only: %i[new create index]
 end
