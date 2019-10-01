c8537cb01c392aabcc93be11f5d2495645e769fd
stickler-ci <support@stickler-ci.com>
2019-10-01 20:01:18 +0000

    Fixing style errors.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,22 +5,22 @@ class UserController < ApplicationController
     @user = User.new
   end
 
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       remember_cookies @user
-      flash.now[:info] = "Account successfully created!"
+      flash.now[:info] = 'Account successfully created!'
       redirect_to root_url
     else
       render 'new'
     end
   end
 
-  private 
+  private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, 
-                                  :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
